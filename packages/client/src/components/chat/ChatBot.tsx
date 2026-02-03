d87e6bfaf4a71541fb51d5d3aaffee96563a8197
Sushyam Nagallapati <sushyamraju@gmail.com>
2026-02-03 11:43:27 -0500

    Extract Typing Indicator component

--- packages/client/src/components/chat/ChatBot.tsx
+++ packages/client/src/components/chat/ChatBot.tsx
@@ -2,8 +2,9 @@ import axios from 'axios';
 import ReactMarkdown from 'react-markdown';
 import { useForm } from 'react-hook-form';
 import { FaArrowUp } from 'react-icons/fa';
-import { Button } from './ui/button';
+import { Button } from '../ui/button';
 import { useEffect, useRef, useState } from 'react';
+import TypingIndicator from './TypingIndicator';
 
 type FormData = {
    input: string;
@@ -85,13 +86,7 @@ const ChatBot = () => {
                   <ReactMarkdown>{message.content}</ReactMarkdown>
                </div>
             ))}
-            {isBotTyping && (
-               <div className="flex self-start gap-1 px-3 py-3 bg-gray-200 rounded-xl">
-                  <div className="w-2 h-2 rounded-full bg-gray-800 animate-pulse"></div>
-                  <div className="w-2 h-2 rounded-full bg-gray-800 animate-pulse [animation-delay: 0.2s]"></div>
-                  <div className="w-2 h-2 rounded-full bg-gray-800 animate-pulse [animation-delay: 0.4s]"></div>
-               </div>
-            )}
+            {isBotTyping && <TypingIndicator />}
             {error && <p className="text-red-500">{error}</p>}
          </div>
          <form
